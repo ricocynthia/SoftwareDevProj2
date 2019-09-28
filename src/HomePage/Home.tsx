61e49fe3d7a2b90fdec53f33f776533c913b5d23
ricocynthia <ricocynthia5@gmail.com>
2019-09-27 19:33:16 -0500

    got the menu and color set up of home page

---
+++ src/HomePage/Home.tsx
@@ -0,0 +1,35 @@
+import React from 'react';
+import Assignment from '@material-ui/icons/Assignment';
+import SideDrawer from './SideDrawer';
+import InventoryUI from '../DrawerCategories/InventoryUI';
+import { Switch, BrowserRouter as Router, Route } from 'react-router-dom';
+
+
+export default function Home(props: any) {
+  const [userUIView, setUserUIView] = React.useState('')
+  const handleUserUIViewChange = (viewOnApp: string) => {
+    setUserUIView(viewOnApp)
+  }
+  return (
+    <div>
+
+    <Router>
+        <Switch>
+
+        <SideDrawer handleUserUIViewChange={handleUserUIViewChange} userUIView={userUIView} />
+
+            <Route path="/inventory"  />
+
+        </Switch>
+    </Router>
+
+
+        {/* Future components:
+            <OrderUI />
+            <AcountUI /> 
+        */}
+
+
+    </div>
+  );
+}
