988fdcb2088f297ae135a6252968e9ae09ccb78c
ricocynthia <ricocynthia5@gmail.com>
2019-10-11 12:21:27 -0500

    Basic components: still need to add elements

---
+++ src/Inventory/InventoryUI.tsx
@@ -0,0 +1,13 @@
+import React from 'react';
+
+export default function InventoryUI() {
+  
+  return (
+    <div>
+        <h1> INVENTORY</h1>
+
+        {console.log('you are on inventory view')}
+
+    </div>
+  );
+}
