00ac3dbc18ac7eeab7eae10fcab03166a4c74d5c
ricocynthia <ricocynthia5@gmail.com>
2019-10-11 12:10:49 -0500

    Implemented auth0 for user login

--- src/Login/Login.tsx
+++ src/Login/Login.tsx
@@ -2,11 +2,11 @@ import React from 'react';
 import {
     Button, Container,
     Typography, makeStyles,
-    Divider, TextField, Checkbox,
-    FormControlLabel, Theme, createStyles
+    Divider, Theme, createStyles
 } from '@material-ui/core';
-import { Link } from 'react-router-dom';
 import LockIcon from '@material-ui/icons/Lock';
+import { useAuth0 } from "../react-auth0-wrapper";
+import { withRouter } from 'react-router-dom';
 
 const useStyles = makeStyles((theme: Theme) =>
     createStyles({
@@ -27,7 +27,7 @@ const useStyles = makeStyles((theme: Theme) =>
         },
         containerClass: {
             backgroundColor: 'white',
-            height: '500px',
+            height: '300px',
             boxShadow: '5px 5px 5px #777777',
             alignContent: 'center'
         },
@@ -36,81 +36,54 @@ const useStyles = makeStyles((theme: Theme) =>
             marginBottom: '16px',
             fontWeight: 600
         },
-        inputSpacing: {
-            marginBottom: "20px"
-        },
         divSpacing: {
             marginTop: '10px'
         },
         button: {
-            width: "350px",
+            width: "250px",
             marginLeft: "100px",
-            marginRight: "100px"
+            marginRight: "100px",
+            backgroundColor: "#908172"
         },
-        forgotPassword: {
-            textTransform: 'none',
+        signInButton: {
+            textDecoration: 'none',
+            color: 'black',
+            textTransform: 'capitalize'
+
         }
     }),
 );
 
 const Login = (props: any) => {
     const classes = useStyles();
+    const { isAuthenticated, loginWithRedirect } = useAuth0();
+    const { history } = props
 
     return (
         <div className={classes.backgroundStyling}>
             <header className={classes.headerClass}>
                 <Container className={classes.containerClass} maxWidth="sm">
-                    <img src={`https://software-dev-proj.s3-us-west-1.amazonaws.com/Maverick_Manufacturing_Logo.png`} style={{width: "400px"}} />
+                    <img src={`https://software-dev-proj.s3-us-west-1.amazonaws.com/Maverick_Manufacturing_Logo.png`} style={{ width: "400px" }} />
                     <Divider />
-                        {console.log(props)}
                     <div style={{ marginTop: "10px" }}>
                         <LockIcon />
-                        <Typography variant="h6" style={{ marginBottom: "6px" }}> Sign In </Typography>
+                        <Typography variant="h6" style={{ marginBottom: "6px" }}> Welcome to ShopFlo! </Typography>
 
-                        <div className={classes.inputSpacing}>
-                            <TextField
-                                variant="outlined"
-                                label="username or employee ID"
-                                required
-                                style={{ width: "350px" }} />
-                        </div>
+                        
 
-                        <div className={classes.divSpacing}>
-                            <TextField
-                                variant="outlined"
-                                type='password'
-                                label="password"
-                                required
-                                style={{ width: "350px" }} />
-                        </div>
-
-                        <div>
-                            <FormControlLabel
-                                control={
-                                    <Checkbox
-                                        color="default"
-                                        value="Remember me"
-                                        inputProps={{
-                                            'aria-label': 'checkbox with default color',
-                                        }}
-                                    />
-                                }
-                                label="Remember me"
-                            />
-                        </div>
-
-                        <div className={classes.divSpacing}>
-                            <Button variant='contained' className={classes.button}>
-                                <Link to='/home'> Sign In </Link>    
+                        {!isAuthenticated && (
+                            <div className={classes.divSpacing}>
+                                <Button variant='contained' className={classes.button} onClick={() => loginWithRedirect({})}>
+                                    Log in
                             </Button>
+                            </div>
+                        )}
 
-                            <Button className={classes.forgotPassword} >
-                                <Link to='/resetpassword' style={{textDecoration: 'none'}}> Forgot Password </Link>
-                            </Button>
-                                
-
-                        </div>
-
+                        {isAuthenticated &&
+                            history.push({
+                                pathname: '/home'
+                            })
+                        }
 
                     </div>
 
@@ -120,4 +93,4 @@ const Login = (props: any) => {
     )
 }
 
-export default Login;
+export default withRouter(Login);
